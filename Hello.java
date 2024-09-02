bbd5742ead9dc0dbce91f66e64f21c09e5cc1ffe
Selena Orellana <oselena96@gmail.com>
2024-09-02 12:03:38 -0700

    erased 5 lines

--- Hello.java
+++ Hello.java
@@ -1,11 +1,7 @@
 public class Hello {
     public static void main(String [] args){
         // Testing the stuff I learned 
-        System.out.println("This is my second time riunning this program");
-        System.out.println("I want to save again");
-        System.out.println("Working on adding more lines");
-        System.out.println("adding a line to commit through terminal");
-        // let see if I remember how to commit correctly using the terminal
-        System.out.println("When I try to commit I dont know what I did wrong");
+        System.out.println("This is all I wamt to save");
+  
     }
 }
