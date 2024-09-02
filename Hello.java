ec28578249c78f6feb01df306936ac0f1c62bca9
Selena Orellana <oselena96@gmail.com>
2024-09-02 11:45:08 -0700

    add second line

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String [] args){
         // Testing the stuff I learned 
-        System.out.println("This is my first time riunning this program");
+        System.out.println("This is my second time riunning this program");
+        System.out.println("I want to save again");
     }
 }
