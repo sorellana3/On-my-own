34fba45f0db7a5d830dc1324c07d454701437fdf
Selena Orellana <oselena96@gmail.com>
2024-09-02 12:00:59 -0700

    add new lines

--- Hello.java
+++ Hello.java
@@ -5,5 +5,7 @@ public class Hello {
         System.out.println("I want to save again");
         System.out.println("Working on adding more lines");
         System.out.println("adding a line to commit through terminal");
+        // let see if I remember how to commit correctly using the termal
+        System.out.println(" when i try to commit i dont know what i did wrong");
     }
 }
