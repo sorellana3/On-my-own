1f0d263b352b8519afa3ee2709463854b62e645d
Selena Orellana <oselena96@gmail.com>
2024-09-02 11:11:19 -0700

    Modify

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-public class Hello{
-    public static void main(String[] args){
-        System.out.println("Hello.World\n");
+public class Hello {
+    public static void main(String [] args){
+        // Testing the stuff I learned 
+        System.out.println("This is my first time riunning this program");
     }
 }
